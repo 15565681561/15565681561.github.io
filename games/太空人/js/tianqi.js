d8c465a33566e385cf78dd3bbb35d8e2aa5d3169
15565681561 <74585605+15565681561@users.noreply.github.com>
2021-03-25 16:32:09 +0800

    aaa

---
+++ games/太空人/js/tianqi.js
@@ -0,0 +1,23 @@
+$(function(){
+			$.ajax({
+				type: "GET",
+				/*
+				*citykey城市id
+				* */
+				url: "http://wthrcdn.etouch.cn/weather_mini?citykey=101010700",
+				success: function(results){
+					
+				var data = jQuery.parseJSON(results); 
+				console.log(data)
+				  $("#fengxiang").html(data.data.forecast[0].fengxiang);
+				  $("#type").html(data.data.forecast[0].type);
+				  $("#wendu").html(data.data.wendu);
+				  var str=data.data.forecast[0].high.substr(2)
+				  $("#high").html(str);
+				  var str1=data.data.forecast[0].low.substr(2)
+				  $("#low").html(str1);
+				},error: function(error){
+          
+				}
+			});
+		})
